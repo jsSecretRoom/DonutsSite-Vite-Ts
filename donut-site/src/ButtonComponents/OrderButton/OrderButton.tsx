24c9e36f662ae4e32824842f82af42e3a63cefc7
Ruslan <g.ruslan.rus@gmail.com>
2023-10-27 16:06:47 +0300

    v16

--- donut-site/src/ButtonComponents/OrderButton/OrderButton.tsx
+++ donut-site/src/ButtonComponents/OrderButton/OrderButton.tsx
@@ -5,23 +5,34 @@ import { setTogleSpeciallModalBox, setOrerSpeciallCollection } from '../../redux
 
 
 function OrderButton() {
-
     const dispatch = useDispatch();
-    
+  
     const toggleModal = useSelector((state) => state.togllebutton.togleSpeciallModalBox);
-
-    let speciallCollection = useSelector((state) => state.getcollection.speciallCollectionBox);
-    let prototipespeciallCollection = useSelector((state) => state.getcollection.speciallCollectionOrer);
-
+    const speciallCollection = useSelector((state) => state.getcollection.speciallCollectionBox);
+  
     const toggleModall = () => {
-        dispatch(setTogleSpeciallModalBox(!toggleModal));
-        let speciallCollectionCopy = [...speciallCollection];
-        dispatch(setOrerSpeciallCollection([...prototipespeciallCollection, speciallCollectionCopy]));
+      dispatch(setTogleSpeciallModalBox(!toggleModal));
+  
+      // Получаем текущие данные из localStorage
+      const savedData = localStorage.getItem('speciallCollectionOrer');
+      let specialProducts = [];
+  
+      if (savedData) {
+        specialProducts = JSON.parse(savedData);
+      }
+  
+      // Проверяем, что speciallCollection не пуст и существует
+      if (speciallCollection && speciallCollection.length > 0) {
+        // Добавляем speciallCollection в общий массив
+        specialProducts.push(speciallCollection);
+        // Сохраняем обновленный массив в localStorage
+        localStorage.setItem('speciallCollectionOrer', JSON.stringify(specialProducts));
+      }
     }
-    
-    return ( 
-        <button onClick={toggleModall}><h4>Show order</h4></button>
+  
+    return (
+      <button onClick={toggleModall}><h4>Show order</h4></button>
     );
-}
-
-export default OrderButton;
+  }
+  
+  export default OrderButton;
--- donut-site/src/components/ModalBag/SpecialCard/SpecialCard.tsx
+++ donut-site/src/components/ModalBag/SpecialCard/SpecialCard.tsx
@@ -1,19 +1,19 @@
 import './SpecialCard.scss';
 function SpecialCard({ speciallsubArray }) {
-    console.log(speciallsubArray);
+    
     return (
         <>
             {speciallsubArray.map((item, itemIndex) => (
                 <div key={itemIndex} className='special-order'>
                     <div className='special-img'>
-                        <img src={item[itemIndex].basketitem.oneimg} alt="oneimg" />
+                        <img src={item.basketitem.oneimg} alt="oneimg" />
                     </div>
                     <div className='info-product-container'>
                         <div className='special-info'>
-                            <p>{item[itemIndex].basketitem.info}</p>
+                            <p>{item.basketitem.info}</p>
                         </div>
                         <div className='special-price-info'>
-                            <p>Local price: {item[itemIndex].basketitem.price}</p>
+                            <p>Local price: {item.basketitem.price}</p>
                             <p>Count: {item.specialcount}</p>
                         </div>
                     </div>
